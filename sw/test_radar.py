a5afe3ec345db2d9c65f3c164fff351010c94a72
Beat Besmer <beat.besmer@gmail.com>
2026-04-22 11:38:48 +0200

    Clean up radar test

--- sw/test_radar.py
+++ sw/test_radar.py
@@ -1,24 +1,13 @@
-# HLK-LD2410 B et C  (Microwave-based human/object presence sensor) 
-#rev 1 DUCROS christian janvier 2024 à partir du fichier 
-#rev 1 - shabaz - May 2023
-
-#programme principal
 from machine import Pin, UART
 import utime
 import ld2410
 
-#led if human_detection
-# board LED on the Pi Pico is 25
-# board LED on the Pico W is 'LED'
 boardled = Pin(13, Pin.OUT)
 boardled.off()
 
 print('-----------CONFIGURATION----------------')
 uart1 = UART(1, baudrate = 256000, tx=Pin(27), rx=Pin(12), timeout = 3)
-print(uart1)
-
 human_sensor = ld2410.LD2410(uart1)
-print('----------------------------------------')
 human_sensor.enable_config()
 #human_sensor.Maximum_distance_gate_and_unoccupied_duration_parameters_configuration(2,2,50) # 2-->8,2-->8,0-->65535 
 #human_sensor.read_parameter()
@@ -33,11 +22,10 @@ human_sensor.get_mac_address()
 human_sensor.distance_resolution_setting(0x0000)
 #human_sensor.query_distance_resolution_setting()
 human_sensor.end_config()
-
 #human_sensor.enable_engineering_mode()
 #human_sensor.end_engineering_mode()
 
-utime.sleep(5) #debug pour lire les rapports de configuration
+utime.sleep(5)
 
 print('-----------DECTECTION----------------')
 while True: 
