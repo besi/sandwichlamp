bf47c5a10278585a021a54d15de6b6e2b460d251
Beat Besmer <beat.besmer@gmail.com>
2026-04-30 14:56:58 +0200

    Initial state of the main.py

---
+++ sw/main.py
@@ -0,0 +1,43 @@
+from machine import Pin
+import asyncio
+from primitives import ESP32Touch
+
+touch1_pin = 14
+touch2_pin = 32
+touch3_pin = 4
+
+
+ESP32Touch.threshold(70)  # optional
+lamp = machine.Pin(13, machine.Pin.OUT)
+
+mode = 1
+
+def mode1():
+    global mode
+    mode = 1
+    lamp.value(1-lamp.value())
+    
+def mode2():
+    global mode
+    mode = 2
+    import test_neopixel.py
+    lamp.off()
+    
+def mode3():
+    global mode
+    mode = 3
+    lamp.off()
+
+async def main():
+    t1 = ESP32Touch(Pin(touch1_pin), suppress=True)
+    t1.press_func(lambda : mode1())
+    t2 = ESP32Touch(Pin(touch2_pin), suppress=True)
+    t2.press_func(lambda : mode2())
+    t3 = ESP32Touch(Pin(touch3_pin), suppress=True)
+    t3.press_func(lambda : mode3())
+
+    while True:
+        await asyncio.sleep(1)
+
+asyncio.run(main())
+
