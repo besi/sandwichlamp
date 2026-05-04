dabff88e39c1fc702028ec3935a62e20cb3d2108
Beat Besmer <beat.besmer@gmail.com>
2026-05-04 17:57:49 +0200

    Update the main program in order to run several different modes

--- sw/main.py
+++ sw/main.py
@@ -1,44 +1,101 @@
-from machine import Pin
+from machine import Pin, PWM
 import asyncio
-from primitives import ESP32Touch
+from primitives import ESP32Touch, Pushbutton
+from  neopixel import NeoPixel
+pixelCount = 44
+np = NeoPixel(Pin(18), pixelCount)
 
 touch1_pin = 14
 touch2_pin = 32
 touch3_pin = 4
 
-ESP32Touch.threshold(80)
-lamp = Pin(13, Pin.OUT)
+button_a_pin = 34
+button_b_pin = 33
+button_c_pin = 39
+button_d_pin = 36
 
+enable12v_pin = 13
+
+pwm_start = 610
+pwm_end = 1020
+pwm_steps = (pwm_end-pwm_start)/20
+
+ESP32Touch.threshold(90)
+
+pwm = PWM(Pin(enable12v_pin), freq=1000)          # create a PWM object on a pin
+
+
+mode_max = 2
 mode = 1
+intensity = 10 # 0 .. 20
 
-def mode1():
-    global mode
-    mode = 1
-    print("toggle lamp")
-    lamp.value(1-lamp.value())
-    
-def mode2():
-    global mode
-    mode = 2
-    import test_neopixel.py
-    lamp.off()
-    
-def mode3():
-    global mode
-    mode = 3
-    lamp.off()
+def down():
+    global intensity
+    intensity = max(0, intensity -1 )
+    print(f"Down {intensity}")
 
+    update_mode(False)
+
+def up():
+    global intensity
+    intensity = min(20, intensity + 1)
+    print(f"Up {intensity}")
+
+    update_mode(False)
+
+def update_mode(modeChanged = True):
+    global mode
+    global intensity
+    
+    print(f"Mode = {mode}")
+    if mode != 1 and modeChanged:
+        pwm.duty(0)
+    if mode == 1:
+        if modeChanged and pwm.duty() > 1:
+            pwm.duty(0)
+        else:
+            pwm.duty(int(intensity * pwm_steps)+pwm_start)
+        
+
+def change_mode():
+    global mode
+    if mode == mode_max:
+        mode = 1
+    else:
+        mode += 1
+    update_mode()
+            
+def button(number):
+    print(f"Pressed Button #{number}")
+    if number == 1:
+        np.fill((25,0,0))
+    elif number == 2:
+        np.fill((0,25,0))
+    elif number == 3:
+        np.fill((0,0,25))
+    elif number == 4:
+        np.fill((0,0,0))
+    np.write()
+    
 async def main():
     t1 = ESP32Touch(Pin(touch1_pin), suppress=True)
-    t1.press_func(lambda : mode1())
+    t1.release_func(lambda : down())
     t2 = ESP32Touch(Pin(touch2_pin), suppress=True)
-    t2.press_func(lambda : mode2())
+    t2.release_func(lambda : change_mode())
     t3 = ESP32Touch(Pin(touch3_pin), suppress=True)
-    t3.press_func(lambda : mode3())
+    t3.release_func(lambda : up())
 
+    pb1 = Pushbutton(Pin(button_a_pin, Pin.IN, Pin.PULL_UP))
+    pb1.press_func(button, (1,))
+    pb2 = Pushbutton(Pin(button_b_pin, Pin.IN, Pin.PULL_UP))
+    pb2.press_func(button, (2,))
+    pb3 = Pushbutton(Pin(button_c_pin, Pin.IN, Pin.PULL_UP))
+    pb3.press_func(button, (3,))
+    pb4 = Pushbutton(Pin(button_d_pin, Pin.IN, Pin.PULL_UP))
+    pb4.press_func(button, (4,))
+
+    update_mode()
     while True:
         await asyncio.sleep(1)
 
 asyncio.run(main())
-
-
